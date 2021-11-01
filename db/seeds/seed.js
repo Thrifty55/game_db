78aa632fc78d1d2336a658d7da95d306e5f610e2
Laura Thrift <laura.thrift@hotmail.co.uk>
2021-11-01 12:00:54 +0000

    completed seeding the table and setting up files

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,128 @@
+const db = require("../connection");
+const format = require("pg-format");
+
+const ENV = process.env.NODE_ENV || "development";
+const categories = require(`../data/${ENV}-data/categories`);
+const comments = require(`../data/${ENV}-data/comments`);
+const reviews = require(`../data/${ENV}-data/reviews`);
+const users = require(`../data/${ENV}-data/users`);
+
+
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+  return db
+  .query(`DROP TABLE IF EXISTS comments;`)
+  .then(() => db.query(`DROP TABLE IF EXISTS reviews;`))
+  .then(() => db.query(`DROP TABLE IF EXISTS users;`))
+  .then(() => db.query(`DROP TABLE IF EXISTS categories;`))
+  .then(() =>
+    db.query(`
+    CREATE TABLE categories (
+      slug VARCHAR PRIMARY KEY,
+      description VARCHAR NOT NULL
+    );`)
+  )
+  .then(() =>
+    db.query(`
+      CREATE TABLE users (
+        username VARCHAR PRIMARY KEY,
+        avatar_url VARCHAR NOT NULL,
+        name VARCHAR(255) NOT NULL
+      );
+      `)
+  )
+  .then(() =>
+  db.query(`
+    CREATE TABLE reviews (
+      review_id SERIAL PRIMARY KEY,
+      title VARCHAR NOT NULL,
+      review_body VARCHAR NOT NULL,
+      designer VARCHAR NOT NULL,
+      review_img_url VARCHAR NOT NULL DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      votes INT NOT NULL DEFAULT 0,
+      category VARCHAR REFERENCES categories(slug),
+      owner VARCHAR REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    `)
+)
+.then(() =>
+db.query(`
+  CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    author VARCHAR REFERENCES users(username),
+    review_id INT REFERENCES reviews(review_id),
+    votes INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    body VARCHAR NOT NULL
+  );
+  `)
+)
+.then(() => {
+  const categoriesString = format(
+    `
+    INSERT INTO categories
+      (slug, description)
+    VALUES %L
+    RETURNING *;
+    `,
+    categories.map((category) => [category.slug, category.description])
+  );
+  return db.query(categoriesString);
+})
+.then(() => {
+  const usersString = format(
+    `
+    INSERT INTO users
+      (username, avatar_url, name)
+    VALUES %L
+    RETURNING *;
+    `,
+    users.map((user) => [user.username, user.avatar_url, user.name])
+  );
+  return db.query(usersString);
+})
+.then(() => {
+  const reviewsString = format(
+    `
+    INSERT INTO reviews
+      (title, review_body, designer, review_img_url, votes, category, owner, created_at)
+    VALUES %L
+    `,
+    reviews.map(
+      ({title, review_body, designer, review_img_url, votes, category, owner, created_at}) => [
+        title,
+        review_body,
+        designer,
+        review_img_url,
+        votes,
+        category,
+        owner,
+        created_at
+      ]
+    )
+  );
+  return db.query(reviewsString);
+})
+.then(() => {
+  const commentsString = format(
+    `
+    INSERT INTO comments
+      (author, review_id, votes, created_at, body)
+    VALUES %L
+    `,
+    comments.map(
+      ({author, review_id, votes, created_at, body}) => [
+        author,
+        review_id,
+        votes,
+        created_at,
+        body
+      ]
+    )
+  );
+  return db.query(commentsString);
+});
 };
 
 module.exports = seed;
