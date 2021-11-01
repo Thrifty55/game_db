1568639b6de7f95c7bd14220675c91d31b9e7482
Laura Thrift <laura.thrift@hotmail.co.uk>
2021-11-01 12:02:22 +0000

    created files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,6 @@
+const request = require("supertest");
 const db = require('../db/connection.js');
+const app = require("../app");
 const testData = require('../db/data/test-data/index.js');
 const { seed } = require('../db/seeds/seed.js');
 
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.statusCode) res.status(err.statusCode).send({ err: err.msg });
+  else {
+    console.log("Error middleware", err);
+    res.status(500).send({ err: "Something went wrong" });
+  }
+});
+
+module.exports = app;
